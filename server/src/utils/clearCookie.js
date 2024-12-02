e823d93142f2fe391989baae03ecc7e410dd4f55
moshzyc <moshezyc@gmail.com>
2024-12-02 14:38:16 +0200

    cookie test2

--- server/src/utils/clearCookie.js
+++ server/src/utils/clearCookie.js
@@ -1,15 +1,15 @@
 export function clearCookie(res, cookieName) {
-  // res.cookie(cookieName, "", {
-  //   httpOnly: true,
-  //   secure: process.env.NODE_ENV === "production", // secure ב-HTTPS בלבד
-  //   sameSite: "lax", // בהתאם לצורך
-  //   expires: new Date(0), // תאריך תפוגה ישן מנקה את העוגייה
-  // })
   res.cookie(cookieName, "", {
     httpOnly: true,
-    secure: true,
-    sameSite: "None", // חשוב לשלוח בין דומיינים שונים
-    partitioned: true, // הגדרת ה-cookie כ-Partitioned
-    expires: new Date(0), // תאריך תפוגה ישן
+    secure: process.env.NODE_ENV === "production", // secure ב-HTTPS בלבד
+    sameSite: "lax", // בהתאם לצורך
+    expires: new Date(0), // תאריך תפוגה ישן מנקה את העוגייה
   })
+  // res.cookie(cookieName, "", {
+  //   httpOnly: true,
+  //   secure: true,
+  //   sameSite: "None", // חשוב לשלוח בין דומיינים שונים
+  //   partitioned: true, // הגדרת ה-cookie כ-Partitioned
+  //   expires: new Date(0), // תאריך תפוגה ישן
+  // })
 }
