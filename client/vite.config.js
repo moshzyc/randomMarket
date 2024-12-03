8049cc8e56d98fed67009d3083cc8f32aaa79686
moshzyc <moshezyc@gmail.com>
2024-12-03 15:19:35 +0200

    respons update 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    manifest: true, // יוצר manifest לבקרת גירסאות
+  },
+  server: {
+    hmr: true, // Hot Module Replacement
+  },
 })
